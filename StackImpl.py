1409bc73c4e1e775f970866da3196995dfcb3228
aryacs <aryanchandrapal@gmail.com>
2024-11-13 11:45:29 +0530

    code added

--- StackImpl.py
+++ StackImpl.py
@@ -1,1 +1,21 @@
+# initial empty stack
+stk = []
 
+# append() function to push
+# element in the stk
+stk.append('x')
+stk.append('y')
+stk.append('z')
+
+print(stk)
+
+# pop() function to pop
+# element from stk in
+# LIFO order
+print('Elements poped from Stack:')
+print(stk.pop())
+print(stk.pop())
+print(stk.pop())
+
+print('Stack after deletion...')
+print(stk)
